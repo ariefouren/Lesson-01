be0bdace6bf8adbc4290b61b73a3979e8f1ad58e
ariefouren <aporan@ono.ac.il>
2022-10-25 22:39:15 +0300

    xx

---
+++ arithmetics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+    cout << "17 / 3 = " << (17/3) << endl;
+    cout << "17 / 3.0 = " << (17/3.0) << endl;
+
+    cout << "17 % 3 = " << (17%3) << endl;
+
+    system("pause");
+    return 0;
+}
---
+++ casting.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+    char ch = 'A';
+
+    cout << "ch           : " << ch << endl;
+    cout << "(int) ch     : " << (int) ch << endl;
+    
+    cout << "(char) 70    : " << (char) 70 << endl;
+    
+    system("pause");
+    return 0;
+}
